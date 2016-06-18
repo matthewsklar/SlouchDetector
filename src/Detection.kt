8acca65a46d2db6aa7a147a17a6d0a5230303af1
Matthew Sklar <matthewsklar227@gmail.com>
2016-06-18 17:43:54 -0400

    Initial Commit

---
+++ src/Detection.kt
@@ -0,0 +1,17 @@
+/**
+ * @author Matthew Sklar
+ * @version 0.1.0
+ * @since 6/18/2016
+ */
+
+class Detector {
+    private final var FACE_CASCADE_NAME: String = "util/haarcascade_frontalface_alt.xml"
+
+    init {
+
+    }
+
+    fun load() {
+
+    }
+}
---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+import org.opencv.core.Core
+
+/**
+ * @author Matthew Sklar
+ * @version 0.1.0
+ * @since 6/18/2016
+ */
+
+fun main(args: Array<String>) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME) //opencv_java2413
+}
